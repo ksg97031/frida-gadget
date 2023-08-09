b0ecc9c7d11186a392c8413b2b5ea39f5c7f70c6
ksg97031 <ksg97031@gmail.com>
2023-08-09 23:35:00 +0900

    v1.3.1

--- scripts/__version__.py
+++ scripts/__version__.py
@@ -1,6 +1,6 @@
 """frida-gadget version information."""
 __title__ = "frida-gadget"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __description__ = "Automated Frida Gadget injection tool"
 __url__ = "https://github.com/ksg97031/frida-gadget"
 __author__ = "ksg97031"
